07c00dfb6f2bb4bf23d952848661142e73ae0647
EricZYLuo <ericzyluo2002@gmail.com>
2022-01-25 20:43:12 -0500

    Finished the first 3 pages

--- ar_beta/lib/main.dart
+++ ar_beta/lib/main.dart
@@ -5,97 +5,194 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext ctxt) {
-    return new MaterialApp(
-      home: new FirstScreen(),
+    return const MaterialApp(
+      home: FirstScreen(),
     );
   }
 }
 
 class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
 
   @override
   Widget build (BuildContext ctxt) {
-    return new Column(
-        children: <Widget>[
+    return Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFFFCF8F1),
+        ),
+        child: Column(
+          children: <Widget>[
 
-          Expanded(
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: Container(
-                padding: const EdgeInsets.all(16.0),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new SecondScreen()),
-                    );
-                  },
-                  child: const Text('Next', style: TextStyle(color: Colors.black)),
+            Container(
+              margin: const EdgeInsets.only(top:70.0),
+              child: Image.asset('assets/images/FirstPageLogo.jpg')
+            ),
+
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Beauty Lives Here',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: 'times-new-roman', fontStyle: FontStyle.normal, fontWeight: FontWeight.normal, color: Colors.black, fontSize: 42, decoration: TextDecoration.none)
+              )
+            ),
+
+            Container(
+                padding: const EdgeInsets.only(top:10.0, left: 75.0, right: 75.0),
+                child: const Text(
+                    'Discover our latest beauty trends & product releases',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontFamily: 'calibri', fontStyle: FontStyle.normal, fontWeight: FontWeight.normal, color: Colors.black, fontSize: 18, decoration: TextDecoration.none)
+                )
+            ),
+
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(ctxt, MaterialPageRoute(builder: (ctxt) => SecondScreen()),
+                      );
+                    },
+                    child: const Text('Next', style: TextStyle(color: Colors.black)),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
     );
   }
 }
 
 class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
   @override
   Widget build (BuildContext ctxt) {
-    return new Column(
-      children: <Widget>[
+    return Container(
+      margin: const EdgeInsets.all(0.0),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+          image: AssetImage('assets/images/SecondPageImage.jpg'),
+          fit: BoxFit.fitWidth,
+        ),
+      ),
 
-        Expanded(
-          child: Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new ThirdScreen()),
-                  );
-                },
-                child: const Text('Next', style: TextStyle(color: Colors.black)),
+        child: Container(
+          margin: const EdgeInsets.only(top: 495.0),
+          decoration: const BoxDecoration(
+            color: Color(0x77EADACF)
+          ),
+          child: Column(
+            children: <Widget>[
+              Container (
+                margin: const EdgeInsets.only(top:40.0),
+                child: const Text(
+                  'Exclusive Offers',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontFamily: 'times-new-roman', fontStyle: FontStyle.normal, fontWeight: FontWeight.normal, color: Colors.black, fontSize: 42, decoration: TextDecoration.none)
+                ),
               ),
-            ),
+
+              Container(
+                  padding: const EdgeInsets.only(top:20.0, left: 75.0, right: 75.0),
+                  child: const Text(
+                      'Get access to weekly exclusive product offers & deals',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontFamily: 'calibri', fontStyle: FontStyle.normal, fontWeight: FontWeight.normal, color: Colors.black, fontSize: 18, decoration: TextDecoration.none)
+                  )
+              ),
+
+              Container(
+                    padding: const EdgeInsets.only(top:30.0, left:300.0),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(ctxt, MaterialPageRoute(builder: (ctxt) => const ThirdScreen()),
+                        );
+                      },
+                      child: const Text('Next', style: TextStyle(color: Colors.black)),
+                    ),
+                  ),
+            ],
           ),
         ),
-      ],
-    );
+      );
   }
 }
 
 class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({Key? key}) : super(key: key);
+
   @override
   Widget build (BuildContext ctxt) {
-    return new Column(
-      children: <Widget>[
+    return Container(
+        margin: const EdgeInsets.all(0.0),
+        decoration: const BoxDecoration(
+          color: Color(0xFFFBF7F2),
+          image: DecorationImage(
+            image: AssetImage('assets/images/ThirdPageImage.jpg'),
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.topCenter,
+          ),
+        ),
 
-        Expanded(
-          child: Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new FourthScreen()),
-                  );
-                },
-                child: const Text('Next', style: TextStyle(color: Colors.black)),
+        child: Column(
+          children: <Widget>[
+
+            Container(
+                padding: const EdgeInsets.only(top:540.0),
+                child: const Text(
+                    'Virtually Try-on',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontFamily: 'times-new-roman', fontStyle: FontStyle.normal, fontWeight: FontWeight.normal, color: Colors.black, fontSize: 42, decoration: TextDecoration.none)
+                )
+            ),
+
+            Container(
+                padding: const EdgeInsets.only(top:20.0, left: 60.0, right: 60.0),
+                child: const Text(
+                    'Try out our best products anywhere in real-time',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontFamily: 'calibri', fontStyle: FontStyle.normal, fontWeight: FontWeight.normal, color: Colors.black, fontSize: 18, decoration: TextDecoration.none)
+                )
+            ),
+
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(ctxt, MaterialPageRoute(builder: (ctxt) => FourthScreen()),
+                      );
+                    },
+                    child: const Text('Next', style: TextStyle(color: Colors.black)),
+                  ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
 
 class FourthScreen extends StatelessWidget {
+  const FourthScreen({Key? key}) : super(key: key);
+
   @override
   Widget build (BuildContext ctxt) {
-    return new Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -106,14 +203,14 @@ class FourthScreen extends StatelessWidget {
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(color: Colors.brown)
+                          side: const BorderSide(color: Colors.brown)
                       )
                   )
               ),
 
               onPressed: () {
                 Navigator.push(ctxt,
-                  new MaterialPageRoute(builder: (ctxt) => new FifthScreen()),
+                  MaterialPageRoute(builder: (ctxt) => FifthScreen()),
                 );
               },
               child: const Text('Sign in with Email'),
@@ -127,14 +224,14 @@ class FourthScreen extends StatelessWidget {
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(color: Colors.brown)
+                          side: const BorderSide(color: Colors.brown)
                       )
                   )
               ),
 
               onPressed: () {
                 Navigator.push(ctxt,
-                  new MaterialPageRoute(builder: (ctxt) => new FifthScreen()),
+                  MaterialPageRoute(builder: (ctxt) => FifthScreen()),
                 );
               },
               child: const Text('Sign in with Facebook'),
@@ -147,9 +244,11 @@ class FourthScreen extends StatelessWidget {
 }
 
 class FifthScreen extends StatelessWidget {
+  const FifthScreen({Key? key}) : super(key: key);
+
   @override
   Widget build (BuildContext ctxt) {
-    return new Column(
+    return Column(
       children: <Widget>[
 
         Expanded(
@@ -159,7 +258,7 @@ class FifthScreen extends StatelessWidget {
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
                 onPressed: () {
-                  Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new FirstScreen()),
+                  Navigator.push(ctxt, MaterialPageRoute(builder: (ctxt) => new FirstScreen()),
                   );
                 },
                 child: const Text('Next', style: TextStyle(color: Colors.black)),
